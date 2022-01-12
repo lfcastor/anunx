b1c0968028475b60a1ba914533941b113dda1c98
Luiz Castorino <lfcastor@yahoo.com.br>
2022-01-12 18:50:44 -0300

    deletar anuncio mais modal confirm

--- pages/api/products/add.js
+++ pages/api/products/add.js
@@ -1,9 +1,10 @@
 import nextConnect from 'next-connect'
-import { post } from '../../src/controllers/products'
+import { post } from '../../../src/controllers/products'
 
 const route = nextConnect()
 
 route.post(post)
+
 
 export default route
 
